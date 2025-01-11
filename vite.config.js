88e2dafba3605f76951cb46eb8be4b64b90d55b4
Matěj Štágl <stagl.matej@centrum.cz>
2025-01-11 14:52:08 +0100

    up deps, swap to umd-react

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import {resolve} from 'path'
 import svgr from 'vite-plugin-svgr';
-import dynamicImportVars  from '@rollup/plugin-dynamic-import-vars';
 
 export default defineConfig({
     define: {
